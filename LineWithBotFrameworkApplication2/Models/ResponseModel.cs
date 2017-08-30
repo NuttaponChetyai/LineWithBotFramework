f368561b30a825ebbeed0ed8d24f11abc8dda5d0
james <james@NUTTAPON>
2017-08-30 09:55:33 +0700

    Add project files.

---
+++ LineWithBotFrameworkApplication2/Models/ResponseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LineWithBotFrameworkApplication2.Models
+{
+    public class ResponseModel
+    {
+        public string messege { get; set; }
+        public bool status { get; set; }
+        public object data { get; set; }
+        public decimal? amount { get; set; }
+        public int total { get; set; }
+    }
+    public class Invoice {
+        public string InvoiceNo { get; set; }
+        public string Comment { get; set; }
+        public decimal? Amount { get; set; } 
+    }
+}
